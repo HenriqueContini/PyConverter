faf4dc909a3ad60565ee932c2a1f889a6df2113b
HC Dev <81761545+HenriqueContini@users.noreply.github.com>
2021-08-30 14:10:38 -0300

    Add files via upload

---
+++ PyConverter/msg_inicial.py
@@ -0,0 +1,4 @@
+def msg_inicial():
+    print('\n-----------------------------------------------------')
+    print('Bem vindo ao PyConverter, um conversor de temperatura')
+    print('-----------------------------------------------------\n')
